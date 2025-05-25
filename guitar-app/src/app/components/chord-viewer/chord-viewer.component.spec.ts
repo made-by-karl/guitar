2600179d9ce131c4f7b5bc3b2d728c1976bdc0da
Karl Rhenius <mail@made-by-karl.de>
2025-05-25 14:09:23 +0000

    use jest instead of karma

--- guitar-app/src/app/components/chord-viewer/chord-viewer.component.spec.ts
+++ guitar-app/src/app/components/chord-viewer/chord-viewer.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ChordViewerComponent } from './chord-viewer.component';
 
@@ -8,7 +11,17 @@ describe('ChordViewerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChordViewerComponent]
+      imports: [ChordViewerComponent, HttpClientTestingModule],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            paramMap: {
+              get: (param: string) => param === 'id' ? 'test-chord' : null
+            }
+          }
+        }
+      }]
     })
     .compileComponents();
 
